0279281c0478fa638d7230f7cd442c734ee60c93
Magdalena <Magdalena@LENOVO>
2016-04-21 15:46:16 +0200

    Initial check in

---
+++ src/main/java/org/ex2/app/App.java
@@ -0,0 +1,9 @@
+package org.ex2.app;
+
+public class App {
+
+	public static void main(String[] args) {
+		new MainWindow();
+	}
+
+}
---
+++ src/main/java/org/ex2/app/MainWindow.java
@@ -0,0 +1,24 @@
+package org.ex2.app;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+@SuppressWarnings("serial")
+public class MainWindow extends JFrame {
+
+	private JPanel contentPane;
+
+	public MainWindow() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
